d4fc7c1211eddb1732f4077e954aee2c56a455b8
jakub41 <lemiszewski@gmx.com>
2019-12-22 02:09:46 +0100

    [WIP] Search functionality

--- src/routes/catalogue.js
+++ src/routes/catalogue.js
@@ -2,11 +2,18 @@
 const express = require("express");
 // Router
 const router = express.Router();
-
 //  Email sender
 const { sendEmail } = require("../utilities/sg_email");
+// Model
+const { catalogue } = require("../models/index.models")
 
-//
+// GET Search catalogue of movies
+router.post("/search?t", async (req, res) => {
+    const t = req.params.t;
+    await catalogue
+        .searchInCatalogue()
+        .
+});
 
 // POST email send
 router.post("/email", async (req, res) => {
